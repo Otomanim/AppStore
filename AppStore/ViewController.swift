593ed1a07073e6fb0ab8a49a23ed849acb1c05d7
Evandro Minamoto <89124140+Otomanim@users.noreply.github.com>
2023-11-21 14:45:01 -0300

    Initial Commit

---
+++ AppStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStore
+//
+//  Created by Evandro Rodrigo Minamoto on 21/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
